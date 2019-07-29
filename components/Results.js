84fb761cd4733d5663fdedb416694d16436c9df1
Dylan Richardson <Dylan.Richardson@libertymutual.com>
2019-07-29 13:14:17 -0400

    Add voter view on bar hover

--- components/Results.js
+++ components/Results.js
@@ -1,12 +1,17 @@
-import React from 'react';
-import { CenteredRow } from '../styles';
+import React, { useState, useEffect } from 'react';
+import { Overlay, Tooltip } from 'react-bootstrap';
 import { BarChart } from 'react-d3-components';
 import _ from 'lodash';
+import { CenteredRow } from '../styles';
 
 const WIDTH_RATIO = 0.6;
 const HEIGHT_RATIO = 0.3;
 
 export const Results = ({ answers, showResults, width, height }) => {
+  const [show, setShow] = useState(false);
+  const [target, setTarget] = useState(null);
+  const [voters, setVoters] = useState([]);
+
   const values = _.sortBy(
     Object.entries(_.countBy(Object.values(answers))).map(([x, y]) => ({
       x,
@@ -14,6 +19,36 @@ export const Results = ({ answers, showResults, width, height }) => {
     })),
     'x'
   );
+
+  useEffect(() => {
+    const bars = document.querySelectorAll('g g .bar');
+
+    const enterListener = ({ target }) => {
+      setShow(true);
+      setTarget(target);
+
+      const index = Array.from(target.parentNode.children).indexOf(target);
+      const selectedVoters = _.keys(
+        _.pickBy(answers, answer => answer === values[index].x)
+      );
+      setVoters(selectedVoters);
+    };
+    const leaveListener = () => {
+      setShow(false);
+    };
+
+    bars.forEach(bar => {
+      bar.addEventListener('mouseenter', enterListener);
+      bar.addEventListener('mouseout', leaveListener);
+    });
+
+    return () => {
+      bars.forEach(bar => {
+        bar.removeEventListener('mouseenter', enterListener);
+        bar.removeEventListener('mouseout', leaveListener);
+      });
+    };
+  });
 
   const showChart = showResults && values.length > 0;
 
@@ -30,6 +65,17 @@ export const Results = ({ answers, showResults, width, height }) => {
   return (
     showChart && (
       <CenteredRow>
+        <Overlay target={target} show={show} placement="right">
+          {props => (
+            <Tooltip
+              {...props}
+              show={props.show.toString()}
+              style={{ ...props.style, maxWidth: '160px' }}
+            >
+              {voters.join(', ')}
+            </Tooltip>
+          )}
+        </Overlay>
         <BarChart
           data={{ values }}
           width={chartWidth}
