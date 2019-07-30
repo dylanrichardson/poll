7f23cc5363cd154b1f571d3b0f8ee5c0b84a3185
Dylan Richardson <Dylan.Richardson@libertymutual.com>
2019-07-30 13:26:40 -0400

    Wrap bar chart labels

--- components/Results.js
+++ components/Results.js
@@ -1,11 +1,17 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { BarChart } from 'react-d3-components';
 import _ from 'lodash';
+import * as d3 from 'd3';
 import { CenteredRow } from '../styles';
 import { Voters } from './';
 
 const WIDTH_RATIO = 0.6;
+const MOBILE_WIDTH_RATIO = 0.8;
 const HEIGHT_RATIO = 0.3;
+const LABEL_LINE_HEIGHT = 67;
+const MOBILE_WIDTH = 768;
+const MAX_LABEL_SIZE = 50;
+const MIN_LABEL_SIZE = 15;
 
 const svgDefs = `
   <defs>
@@ -30,7 +36,41 @@ const svgDefs = `
   </defs>
 `;
 
+function wrapLabel(words, width) {
+  var text = d3.select(this),
+    words = words.split(/\s+/).reverse(),
+    word,
+    line = [],
+    lineNumber = 0,
+    lineHeight = 1.1,
+    y = text.attr('y'),
+    dy = parseFloat(text.attr('dy')),
+    tspan = text
+      .text(null)
+      .append('tspan')
+      .attr('x', 0)
+      .attr('y', y)
+      .attr('dy', dy + 'em');
+  while ((word = words.pop())) {
+    line.push(word);
+    tspan.text(line.join(' '));
+    if (tspan.node().getComputedTextLength() > width) {
+      line.pop();
+      tspan.text(line.join(' '));
+      line = [word];
+      tspan = text
+        .append('tspan')
+        .attr('x', 0)
+        .attr('y', y)
+        .attr('dy', ++lineNumber * lineHeight + dy + 'em')
+        .text(word);
+    }
+  }
+}
+
 export const Results = ({ answers, showResults, width, height }) => {
+  const [labelLines, setLabelLines] = useState(1);
+
   const values = _.sortBy(
     Object.entries(_.countBy(Object.values(answers))).map(([x, y]) => ({
       x,
@@ -41,23 +81,43 @@ export const Results = ({ answers, showResults, width, height }) => {
 
   const showChart = showResults && values.length > 0;
 
+  const wrap = (text, width) => {
+    text.each(function(_, index) {
+      wrapLabel.call(this, values[index].x, width);
+    });
+
+    const lines = _.max(_.map(text[0], 'childNodes.length'));
+
+    setLabelLines(lines);
+  };
+
   useEffect(() => {
     if (showChart) {
-      document
-        .querySelector('#results svg')
-        .insertAdjacentHTML('afterbegin', svgDefs);
+      const barWidth = 138 / _.keys(answers).length + 18;
+
+      d3.selectAll('g.x.axis .tick text').call(wrap, barWidth);
+
+      if (!document.querySelector('#results svg defs')) {
+        document
+          .querySelector('#results svg')
+          .insertAdjacentHTML('afterbegin', svgDefs);
+      }
     }
   });
 
   const numTicks = _.max(_.map(values, 'y'));
 
-  const chartWidth = width < 768 ? width * 0.8 : width * WIDTH_RATIO;
-  const chartHeight = height * HEIGHT_RATIO;
+  const chartWidth =
+    width < MOBILE_WIDTH ? width * MOBILE_WIDTH_RATIO : width * WIDTH_RATIO;
+  const chartHeight =
+    height * HEIGHT_RATIO + (labelLines - 1) * LABEL_LINE_HEIGHT;
 
   const horizontalMargin = chartWidth * 0.1;
   const verticalMargin = chartHeight * 0.05;
 
-  const labelSizes = _.map(values, ({ x }) => Math.min(200 / x.length, 50));
+  const labelSizes = _.map(values, ({ x }) =>
+    Math.max(Math.min(200 / x.length, MAX_LABEL_SIZE), MIN_LABEL_SIZE)
+  );
 
   const labelSizesCSS = labelSizes
     .map(
