ac1d2c4a28e3d70e84cf7b3cab61c52347503a9c
Dylan Richardson <Dylan.Richardson@libertymutual.com>
2019-07-31 09:23:45 -0400

    Add title

--- components/Poll.js
+++ components/Poll.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Col } from 'react-bootstrap';
+import { Col, Row } from 'react-bootstrap';
 import styled from 'styled-components';
 import { Ask, ToggleResults, Answer, Question, Results, Info } from './';
 import { PageContainer, CenteredContainer, CenteredRow } from '../styles';
@@ -7,6 +7,9 @@ import { PageContainer, CenteredContainer, CenteredRow } from '../styles';
 const SMALL = 466;
 const MEDIUM = 768;
 const EXTRA_LARGE = 1200;
+
+const TITLE_MIN_HEIGHT = 440;
+const TITLE_MIN_WIDTH = 830;
 
 const CenteredCol = styled(Col)`
   height: 80%;
@@ -56,46 +59,60 @@ export const Poll = ({
   const isLeader = leader === name;
   const isMobile = width < MEDIUM;
 
+  const showTitle = width > TITLE_MIN_WIDTH && height > TITLE_MIN_HEIGHT;
+
   members.sort();
 
   return (
-    <PageContainer verticalPadding="5vh">
-      <CenteredRow className="align-items-center" style={{ height: '100%' }}>
-        <Info
-          isMobile={isMobile}
-          width={width}
-          members={members}
-          leader={leader}
-          name={name}
-          pin={pin}
-        />
-        <CenteredCol>
-          <CenteredContainer>
-            <Question question={question} />
-            <Ask pin={pin} isLeader={isLeader} isMobile={isMobile} />
-            <Answer
-              pin={pin}
-              name={name}
-              question={question}
-              isLeader={isLeader}
-              ownAnswer={ownAnswer}
-              isMobile={isMobile}
-            />
-            <ToggleResults
-              pin={pin}
-              isLeader={isLeader}
-              showResults={showResults}
-              question={question}
-            />
-            <Results
-              answers={answers}
-              showResults={showResults}
-              width={width}
-              height={height}
-            />
-          </CenteredContainer>
-        </CenteredCol>
-      </CenteredRow>
-    </PageContainer>
+    <>
+      {showTitle && (
+        <Row style={{ position: 'absolute', top: '15px', left: '35px' }}>
+          <Col>
+            <h1 className="display-4">
+              <span style={{ color: 'red' }}>Poll</span>
+              <span style={{ fontWeight: 600 }}>job</span>
+            </h1>
+          </Col>
+        </Row>
+      )}
+      <PageContainer verticalPadding="5vh">
+        <CenteredRow className="align-items-center" style={{ height: '100%' }}>
+          <Info
+            isMobile={isMobile}
+            width={width}
+            members={members}
+            leader={leader}
+            name={name}
+            pin={pin}
+          />
+          <CenteredCol>
+            <CenteredContainer>
+              <Question question={question} />
+              <Ask pin={pin} isLeader={isLeader} isMobile={isMobile} />
+              <Answer
+                pin={pin}
+                name={name}
+                question={question}
+                isLeader={isLeader}
+                ownAnswer={ownAnswer}
+                isMobile={isMobile}
+              />
+              <ToggleResults
+                pin={pin}
+                isLeader={isLeader}
+                showResults={showResults}
+                question={question}
+              />
+              <Results
+                answers={answers}
+                showResults={showResults}
+                width={width}
+                height={height}
+              />
+            </CenteredContainer>
+          </CenteredCol>
+        </CenteredRow>
+      </PageContainer>
+    </>
   );
 };
